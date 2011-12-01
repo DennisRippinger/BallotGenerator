2f1d1ecc86fa7f11fc5f84ff1346194f54970af6
Dennis Rippinger <1452505+DennisRippinger@users.noreply.github.com>
2011-12-01 11:13:32 +0000

    Initial commit

---
+++ BallotGenerator/BallotGenerator/Util/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BallotGenerator
+{
+    public class Configuration
+    {
+        public string pdfLatex;
+        public int packageSize;
+    }
+}
